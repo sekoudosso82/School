cecbf03af6daf22bc8101c2bee206f85bcd1fb2f
sekoudosso82 <sekou.dosso82@gmail.com>
2020-06-25 22:27:26 -0400

    authentication

--- school-back-end/config/routes.rb
+++ school-back-end/config/routes.rb
@@ -54,5 +54,9 @@ Rails.application.routes.draw do
       resources :professors
     end
   end
+
+  post '/signup', to: "students#create"
+  post "/login", to: "auth#login"
+  get '/auto_login', to: 'auth#auto_login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
